e0c6c6f7547ae143f3fae60c522357c7472f3ed4
wxxfe <709197668@qq.com>
2017-11-21 10:20:01 +0800

    11-21

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,17 +2,47 @@ import React,{Component} from 'react';
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import actions from '../../store/actions/home.js'
-
+import './index.less'
 class Home extends Component{
-    componentDidMount(){
-        this.props.getState();
-    }
+
     render(){
         return (
-            <div>
-                {this.props.id}
-                <Link to="/person">个人</Link>
-            </div>
+            <div className="home">
+                <div className="kheader">
+                    <i className="iconfont icon-icon1460189708222"></i>
+                    <div>
+                        <i  className="iconfont icon-fangdajing"></i>
+                        <input type="text" placeholder="搜索菜谱、食材"/>
+                        <i className="iconfont icon-yuyin"></i>
+                    </div>
+
+
+                    <i className="iconfont icon-biji"></i>
+                </div>
+                <div className="main">
+                    <div className="banner">
+
+                        <div><img src={require('./img/home-1.png')} alt=""/>
+                            <p>厨房好物</p>
+                        </div>
+                        <div><img src={require('./img/home-2.png')} alt=""/>
+                            <p>厨房问答</p>
+                        </div>
+                        <div><img src={require('./img/home-3.png')} alt=""/>
+                            <p>排行榜</p>
+                        </div>
+                        <div><img src={require('./img/home-4.png')} alt=""/>
+                            <p>菜谱分类</p>
+                        </div>
+                    </div>
+                    <div className="pic">
+                        <div className="left"><img src={require('./img/qiezi.jpg')} alt=""/><p>本周<br/>流行菜谱</p></div>
+                        <div className="right"><img src={require('./img/jingwu.jpg')} alt=""/><p>查看好友<br/>并关注她们</p></div>
+                    </div>
+                </div>
+
+                </div>
+
         )
     }
 }
