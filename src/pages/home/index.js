9b2df1815856d20e2d6da65400c4a80ab4c18236
wxxfe <709197668@qq.com>
2017-11-21 15:28:03 +0800

    lunbotu

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,8 +3,22 @@ import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import actions from '../../store/actions/home.js'
 import './index.less'
+import Swiper from 'swiper';
 class Home extends Component{
 
+    componentDidMount(){
+         let mySwiper =new Swiper('.swiper-container', {
+             speed: 400,
+             loop: true,
+             autoplay: {
+                 delay: 3000,
+                 disableOnInteraction: false,
+             },
+             pagination: {
+                 el: '.swiper-pagination',
+             }
+         });
+    }
     render(){
         return (
             <div className="home">
@@ -20,6 +34,7 @@ class Home extends Component{
                     <i className="iconfont icon-biji"></i>
                 </div>
                 <div className="main">
+                    <img className="ceng" src={require('./img/ceng.png')} alt=""/>
                     <div className="banner">
 
                         <div><img src={require('./img/home-1.png')} alt=""/>
@@ -38,6 +53,25 @@ class Home extends Component{
                     <div className="pic">
                         <div className="left"><img src={require('./img/qiezi.jpg')} alt=""/><p>本周<br/>流行菜谱</p></div>
                         <div className="right"><img src={require('./img/jingwu.jpg')} alt=""/><p>查看好友<br/>并关注她们</p></div>
+                    </div>
+                    <div className="swiper-container">
+
+                        <div className="swiper-wrapper">
+
+                            <div className="swiper-slide">
+                                <img src={require('./img/morning-bird.png')} alt=""/><span>早餐</span><img src={require('./img/zao.jpg')} alt=""/>
+                            </div>
+                            <div className="swiper-slide">
+                                <img src={require('./img/afternoon-cat.png')} alt=""/><span>午餐</span><img src={require('./img/wu.jpg')} alt=""/>
+                            </div>
+                            <div className="swiper-slide">
+                                <img src={require('./img/evening-noon.png')} alt=""/><span>晚餐</span><img src={require('./img/wan.jpg')} alt=""/>
+                            </div>
+                        </div>
+
+                        <div className="swiper-pagination"></div>
+
+
                     </div>
                 </div>
 
