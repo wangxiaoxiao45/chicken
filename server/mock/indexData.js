b158c66a16b43d9a96b941922b2d523bc4acb25e
wangtianming1994 <68365166@qq.com>
2017-11-21 16:23:03 +0800

    更新首页数据

--- server/mock/indexData.js
+++ server/mock/indexData.js
@@ -84,7 +84,7 @@ module.exports = {
             {
                 indexId: 1,
                 title: "用36道美食，揭开减肥的奥秘",
-                img: "/images/indexImg/second/1.jpg",
+                img: "/images/indexImg/third/1.jpg",
                 special: '专栏',
                 price:'￥19.9',
                 stage:'36期',
@@ -92,37 +92,106 @@ module.exports = {
             },
             {
                 indexId: 2,
-                title: "简单丰盛美好，庄祖宜的中西家常菜",
-                img: "/images/indexImg/second/2.jpg",
+                title: "40道辅食菜谱实战+系统知识详解",
+                img: "/images/indexImg/third/2.jpg",
                 special: '电子书',
-                price:'￥19.9',
-                stage:'103篇',
+                price:'￥9.9',
+                stage:'98篇',
                 detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程，作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
             },
             {
                 indexId: 3,
-                title: "玻璃糖",
-                img: "/images/indexImg/second/3.jpg",
-                teach: '讲师',
-                teachName:'i烘焙',
+                title: "视频|跟着tinrry学烘焙，从此不再失败",
+                img: "/images/indexImg/third/3.jpg",
+                special: '视频专栏',
+                price:'￥9.9',
+                stage:'98篇',
                 detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
             },
             {
                 indexId: 4,
-                title: "最具魅力星球杯蛋糕",
-                img: "/images/indexImg/second/4.jpg",
-                teach: '讲师',
-                teachName:'玉置浩二',
+                title: "两个人的小厨时光",
+                img: "/images/indexImg/third/4.jpg",
+                special: '视频专栏',
+                price:'￥19.9',
+                stage:'68篇',
+                detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
+            },
+
+        ]
+    },
+    four: {
+        title: '市集精选折扣',
+        list: [
+            {
+                indexId: 1,
+                title: "冬天必喝奶薏米麦片猕猴桃粥",
+                img: "/images/indexImg/four/1.jpg",
+                buy: '必买好物',
+                detailText: '猫宁！昨夜大风的北京盖一床被子活生生的冻醒了！ 小汤碗的颜值还不错哦 牛奶薏米麦片猕猴桃粥，好长的名字⋯⋯ 总是纠结...'
+            },
+            {
+                indexId: 2,
+                title: "早餐•2015年12月1日 ",
+                img: "/images/indexImg/four/2.jpg",
+                buy: '必买好物',
+                detailText: '北京的雾霾太可怕了⋯⋯ 据气象台传言今晚十点以后会有风进入北京，逐渐加大，最大风力6级，期待明天霾散天蓝艳阳高照！ #...'
+            },
+            {
+                indexId: 3,
+                title: "巴西分层杯",
+                img: "/images/indexImg/four/3.jpg",
+                buy: '必买好物',
+                detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
+            },
+            {
+                indexId: 4,
+                title: "好利来蛋糕餐具",
+                img: "/images/indexImg/four/4.jpg",
+                buy: '必买好物',
                 detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
             },
             {
                 indexId: 5,
-                title: "弗莱明戈式曲奇甜点",
-                img: "/images/indexImg/second/5.jpg",
-                teach: '讲师',
-                teachName:'任然',
+                title: "巴西好时光咖啡",
+                img: "/images/indexImg/four/4.jpg",
+                buy: '必买好物',
+                detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
+            },
+
+        ]
+    },
+    five:{
+        title: '菜单推荐',
+        list: [
+            {
+                indexId: 1,
+                title: "看一眼就犯馋的甜点",
+                img: "/images/indexImg/five/1.jpg",
+                score:'44道菜',
+                detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
+            },
+            {
+                indexId: 2,
+                title: "好吃到没朋友的乳酪",
+                img: "/images/indexImg/five/2.jpg",
+                score:'166道菜',
+                detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程，作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
+            },
+            {
+                indexId: 3,
+                title: "琵琶",
+                img: "/images/indexImg/five/3.jpg",
+                score:'16道菜',
+                detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
+            },
+            {
+                indexId: 4,
+                title: "超级好吃的韩国大酱汤",
+                img: "/images/indexImg/third/4.jpg",
+                score:'86道菜',
                 detailText: '作为位于北京CBD唯一一家高端生活教室，不论你是零基础厨艺的小白，想用烘培、料理、花异提高生活品质，还是动手大人需要来自世界各地，在领域内颇有建树的专业大师来教授专业性极强的各类课程。'
             },
         ]
-    },
+    }
 };
